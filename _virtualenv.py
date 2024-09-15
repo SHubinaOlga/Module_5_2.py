a4011f7a1b6a62f1450ad10139977e4acd24ad50
SHubinaOlga <olga.shubina85@yandex.ru>
2024-09-15 12:15:19 +0300

    virtualenv.py

--- _virtualenv.py
+++ _virtualenv.py
@@ -3,11 +3,24 @@ class House:    #('ЖК Эльбрус', 30)
         self.name = name
         self.number_of_floors = int(number_of_floors)
 
+    def go_to(self, new_floor: int):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
+            print('"Такого этажа не существует"')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
     def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
         return (f'"{self.name}, кол-во этажей: {self.number_of_floors}"')
+
+#h1 = House('ЖК Горский', 18)
+#h2 = House('Домик в деревне', 2)
+#h1.go_to(5)
+#h2.go_to(10)
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -17,4 +30,3 @@ print(h2)
 
 print(len(h1))
 print(len(h2))
-
